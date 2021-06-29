df7bd521e87fdfb266b4c489b29470c740edacd2
Geraldo Fada <geraldofada.neto@gmail.com>
2021-06-29 13:44:51 -0300

    globalstyle added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
 import 'normalize.css';
+
+import Table from './Table';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: Roboto, Helvetica, Arial, sans-serif
+  }
+`;
 
 const App = () => {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <header>
         <h1>Teste</h1>
       </header>
-    </div>
+
+      <Table />
+    </>
   );
 }
 
